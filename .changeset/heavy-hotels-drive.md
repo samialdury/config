760056947a21ef386ab1dc29bbc1393407f88dcd
Sami Al-Dury <git@aldury.com>
2023-07-02 20:57:26 +0200

    feat: declaration map

---
+++ .changeset/heavy-hotels-drive.md
@@ -0,0 +1,5 @@
+---
+'@samialdury/config': minor
+---
+
+Add declaration map to improve IDE go to definition functionality
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,31 @@
 import { defineConfig } from 'tsup'
+import { spawn } from 'child_process'
 
 export default defineConfig({
     clean: true,
     entry: ['./src/index.ts'],
     format: ['cjs', 'esm'],
-    dts: true,
     bundle: true,
     target: 'es2022',
     outDir: 'dist',
+    onSuccess: async () => {
+        return new Promise((resolve) => {
+            console.log('\nBuild succeeded!\n')
+            console.log('Generating declaration files...\n')
+
+            spawn('tsc', ['--emitDeclarationOnly', '--declaration'], {
+                stdio: 'inherit',
+                shell: true,
+            })
+                .on('error', (err) => {
+                    console.error(err)
+                })
+                .on('exit', (code) => {
+                    if (code === 0) {
+                        console.log('Declaration files generated!')
+                        resolve()
+                    }
+                })
+        })
+    },
 })
